1092a3252d019df327984d7549b641fc869be866
Pato Farey <cfarias@strixsoft.com>
2026-04-08 20:28:47 -0400

    fix mensaje y mail

--- Services/IEmailService.cs
+++ Services/IEmailService.cs
@@ -5,4 +5,6 @@ public interface IEmailService
     Task<(bool success, string? errorMessage)> SendEmailAsync(string to, string subject, string htmlBody);
     Task<(bool success, string? errorMessage)> SendWelcomeEmailAsync(string to, string userName, string userId);
     Task<(bool success, string? errorMessage)> SendPasswordResetCodeAsync(string to, string userName, string resetCode);
+    Task<(bool success, string? errorMessage)> SendOrderConfirmationToBuyerAsync(string to, Models.DTOs.SaleResponse sale, string storeEmail = "");
+    Task<(bool success, string? errorMessage)> SendOrderNotificationToSellerAsync(string to, string storeName, Models.DTOs.SaleResponse sale);
 }
