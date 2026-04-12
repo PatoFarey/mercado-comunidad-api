8013cdb812c5368eec1db228687a393958a51f93
Pato Farey <cfarias@strixsoft.com>
2026-04-12 19:54:31 -0400

    2

--- Services/IOgImageService.cs
+++ Services/IOgImageService.cs
@@ -1,6 +1,9 @@
 namespace ApiMercadoComunidad.Services;
 
+public record ProductOgMeta(string Title, string Description);
+
 public interface IOgImageService
 {
     Task<byte[]> GenerateProductOgImageAsync(string productId);
+    Task<ProductOgMeta> GetProductMetaAsync(string productId);
 }
